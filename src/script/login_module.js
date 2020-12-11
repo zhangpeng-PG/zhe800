dcdbe6f49968c56a7bf417b954e04f79a0a7f861
zhangpeng-PG <405912541@qq.com>
2020-12-11 15:18:22 +0800

    项目第一次提交

---
+++ src/script/login_module.js
@@ -0,0 +1,30 @@
+define([], () => {
+    return {
+        init: function() {
+            const $username = $('#username');
+            const $password = $('#password');
+            const $login = $('#login'); //登录按钮
+
+            $login.on('click', function() {
+                $.ajax({
+                    type: 'post',
+                    url: 'http://localhost:8080/dashboard/JS2010/projectname/php/login.php',
+                    data: {
+                        user: $username.val(),
+                        pass: $password.val()
+                    }
+                }).done(function(data) {
+                    if (!data) { //登录失败
+                        alert('用户名或者密码有误!');
+                        $password.val(''); //密码清空
+                    } else { //登录成功
+                        location.href = 'index.html'; //前端和前端进行页面的通信，相对路径即可，如果是前后端的通信一定是觉对路径。
+                        //存储用户名，方便首页获取。
+                        localStorage.setItem('loginname', $username.val());
+                    }
+                })
+            });
+
+        }
+    }
+})
---
+++ src/script/registry_module.js
@@ -0,0 +1,25 @@
+define([], () => {
+    return {
+        init: function() {
+            const $username = $('#username');
+            const $span = $('span');
+
+            $username.on('blur', function() {
+                $.ajax({
+                    type: 'post',
+                    url: 'http://localhost:8080/dashboard/JS2010/projectname/php/reg.php',
+                    data: {
+                        xingming: $username.val()
+                    }
+                }).done(function(data) { //data就是后端返回的结果
+                    if (!data) { //不存在
+                        $span.css('color', 'green').html('√');
+                    } else { //存在
+                        $span.css('color', 'red').html('该用户名已存在');
+                    }
+                });
+            });
+
+        }
+    }
+})
